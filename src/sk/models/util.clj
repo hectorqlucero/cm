c0b0ce850b2e983c90406c49f9be8923bba8f7cc
Hector Lucero <hectorqlucero@gmail.com>
2021-09-30 17:16:57 -0700

    Restric grid height to 500px

--- src/sk/models/util.clj
+++ src/sk/models/util.clj
@@ -225,7 +225,7 @@
 
 (defn build-table [title url fields]
   [:table.dg
-   {:style "width: 100%;height:auto;"
+   {:style "width: 100%;height:500px;"
     :title title
     :data-options
     (str
