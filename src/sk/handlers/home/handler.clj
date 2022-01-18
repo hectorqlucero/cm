25633b892bd67c9cf98e6f4f9d948180f8e12b73
Hector Lucero <hectorqlucero@gmail.com>
2022-01-17 17:26:40 -0800

    Added strava club statistics

--- src/sk/handlers/home/handler.clj
+++ src/sk/handlers/home/handler.clj
@@ -10,46 +10,31 @@
             [sk.models.util :refer [get-session-id]]))
 
 ;; Start Main
-(def main-sql
-  "SELECT
-   username
-   FROM users
-   WHERE id = ?")
+(defn get-rides0 []
+  (str
+   "
+<iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/1017948/latest-rides/a81e6af10bcf08603aa9226fa75b0b41f600251e?show_rides=false' width='300'></iframe>
+    "))
+(defn get-rides1 []
+  (str
+   "
+<iframe allowtransparency frameborder='0' height='454' scrolling='no' src='https://www.strava.com/clubs/1017948/latest-rides/a81e6af10bcf08603aa9226fa75b0b41f600251e?show_rides=true' width='300'></iframe>
+    "))
 
-(defn get-main-title []
-  [:div {:style "margin-left:20px;
-                margin-right:20px;
-                margin-bottom:20px;"}
-   [:h3 "Bienvenido al sitio Ciclismo Mexicali"]
-   [:ul
-    [:li [:strong "Eventos: "] "Listado de todos los eventos y carreras.  Ej. Paseo Rosarito Ensenada"]
-    [:li [:strong "Rodadas: "] "Rodadas organizadas por grupos o particulares.  Ej. Ver las rodadas de Grupos y confirmar asistencia a rodadas."]
-    [:li [:strong "Aventuras: "] "Aquí podrás escribir acerca de tus aventuras en el ciclismo o en alguna rodada donde participes."]
-    [:li [:strong "Fotos: "] "Aqui puedes ver las fotos de las rodadas de Ciclismo Mexicali"]
-    [:li [:strong "Videos: "] "Aqui puedes ver los de las rodadas de Ciclismo Mexicali"]
-    [:li [:strong "Frases de ciclistas: "] "Aqui puedes ver frases de ciclistas"]
-    [:li [:strong "Talleres: "] "Talleres de reparación/venta de bicicletas"]
-    [:li [:strong "Grupos: "] "Grupos ciclistas ej. Cuadrante Rosita, Sector Ciclista Azul etc..."]
-    [:li [:strong "Entrar: "] "Aquí podrás registrarte como miembro y así crear rodadas para invitar a otros ciclistas.  Las rodadas que hayas creado saldrán en el calendario de rodadas y ciclistas podrán confirmar asistencia y recibirás un correo electronico.  Si cancelas la rodada todos los que confirmaron serán notificados con un correo electrónico que la rodada se canceló"]]
-   [:br] [:br]
-   [:div {:style "margin-left:20px;margin-right:20px;margin-bottom:20px;"}
-    [:p [:a.btn.btn-secondary {:href "https://www.facebook.com/ciclismourbanomexicali"
-                               :target "_blank"} [:strong.text-secondary "Clic aqui para ir a la pagina de facebook."]]]
-    [:p "Este sitio es para todos los ciclistas ya sea de Mexicali o cualquiera que venga a Mexicali por causas de trabajo o vacaciones."]
-    [:hr]
-    [:p "Si tienes alguna sugerencia o necesitas ayuda, estamos para ayudarte"]
-    [:p [:a.btn.btn-secondary {:href "mailto:lucero_systems@fastmail.com"
-                               :target "_blank"} [:stron.text-secondary "Mandame un Correo"]]]]])
+(defn get-rides []
+  [:div.row
+   [:div.col
+    [:p (get-rides0)]]
+   [:div.col (get-rides1)]])
 
 (defn main
   [_]
   (try
     (let [title (:site config)
           ok (get-session-id)
-          content [:div [:span {:style "margin-left:20px;"} (get-main-title)]]]
+          content (get-rides)]
       (application title ok nil content))
     (catch Exception e (.getMessage e))))
-;; End Main
 
 ;; Start Login
 (defn login
